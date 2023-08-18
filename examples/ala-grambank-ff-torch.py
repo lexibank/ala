91d3b8ea8fa26247f98d8ed6374746d5f7947333
Frederic Blum <frederic_blum@eva.mpg.de>
2023-08-18 15:05:43 +0200

    add multiple runs

--- examples/ala-grambank-ff-torch.py
+++ examples/ala-grambank-ff-torch.py
@@ -1,17 +1,18 @@
-from ala import get_wl, get_asjp, training_data, get_gb
-from ala import feature2vec, get_db
+from statistics import mean, stdev
 import numpy as np
-from collections import defaultdict
 import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
+from ala import get_wl, get_asjp, training_data, get_gb
+from ala import feature2vec, get_db
 
 
-results = defaultdict()
+RUNS = 100
+scores = []
 
 lb = get_wl("lexibank.sqlite3")
 asjp = get_asjp()
-wordlists = {k: v for k, v in get_gb("grambank.sqlite3").items()}
+wordlists = {k: v for k, v in get_gb("grambank.sqlite3").items() if k in lb}
 
 # get converter for grambank data
 converter = feature2vec(get_db("grambank.sqlite3"))
@@ -39,116 +40,126 @@ class FF(nn.Module):
         return out
 
 
-control = []
-train, test = training_data(
-        wordlists,
-        {k: v[0] for k, v in get_asjp().items() if k in lb},
-        0.8,
-        3
-        )
+for i in range(RUNS):
+    highest_acc = 0
+    train, test = training_data(
+            wordlists,
+            {k: v[0] for k, v in get_asjp().items() if k in lb},
+            0.8,
+            3
+            )
 
-fam2idx = {fam: i for i, fam in enumerate(train)}
-idx2fam = {v: k for k, v in fam2idx.items()}
-# Create training vector
-training_vec = []
-train_labels = []
+    fam2idx = {fam: i for i, fam in enumerate(train)}
+    idx2fam = {v: k for k, v in fam2idx.items()}
+    # Create training vector
+    training_vec = []
+    train_labels = []
 
-# train: {lng: [data]}
-for fam in train:
-    # iterate through data for each language
-    for lng, data in train[fam].items():
-        # set true label
-        true_labels = [0 for i in range(len(fam2idx))]
-        true_labels[fam2idx[fam]] = 1
+    # train: {lng: [data]}
+    for fam in train:
+        # iterate through data for each language
+        for lng, data in train[fam].items():
+            # set true label
+            true_labels = [0 for i in range(len(fam2idx))]
+            true_labels[fam2idx[fam]] = 1
 
-        # retrieve data to create converter
-        words = [[x[2], x[3]] for x in data.values()]
+            # retrieve data to create converter
+            words = [[x[2], x[3]] for x in data.values()]
 
-        # convert to long vector of sound classes
-        feature_vector = converter(words)
-        training_vec.append(feature_vector)
-        train_labels.append(true_labels)
+            # convert to long vector of sound classes
+            feature_vector = converter(words)
+            training_vec.append(feature_vector)
+            train_labels.append(true_labels)
 
-# Similar vector creation of items in test-set
-test_vec = []
-test_labels = []
-for fam in test:
-    # iterate through all items again
-    for lng, data in test[fam].items():
-        true_labels = [0 for i in range(len(fam2idx))]
-        true_labels = fam2idx[fam]
-        words = [[row[2], row[3]] for row in data.values()]
-        feature_vector = converter(words)
-        test_vec.append(feature_vector)
-        test_labels.append(true_labels)
+    # Similar vector creation of items in test-set
+    test_vec = []
+    test_labels = []
+    for fam in test:
+        # iterate through all items again
+        for lng, data in test[fam].items():
+            true_labels = [0 for i in range(len(fam2idx))]
+            true_labels = fam2idx[fam]
+            words = [[row[2], row[3]] for row in data.values()]
+            feature_vector = converter(words)
+            test_vec.append(feature_vector)
+            test_labels.append(true_labels)
 
-input_dim = len(feature_vector)
-hidden_dim = 4*len(fam2idx)
-output_dim = len(fam2idx)
+    input_dim = len(feature_vector)
+    hidden_dim = 4*len(fam2idx)
+    output_dim = len(fam2idx)
 
-LR = 0.002
-EPOCHS = 500
-BATCH = 10
-ITER = 0
+    LR = 0.002
+    EPOCHS = 500
+    BATCH = 10
+    ITER = 0
 
-train_data = torch.Tensor(np.array(training_vec))
-train_labels = torch.Tensor(np.array(train_labels))
-test_data = torch.Tensor(np.array(test_vec))
-test_labels = torch.Tensor(np.array(test_labels))
+    # Convert data to Tensor
+    train_data = torch.Tensor(np.array(training_vec))
+    train_labels = torch.Tensor(np.array(train_labels))
+    test_data = torch.Tensor(np.array(test_vec))
+    test_labels = torch.Tensor(np.array(test_labels))
 
+    # Load data as TensorDataset
+    train_dataset = TensorDataset(train_data, train_labels)
+    test_dataset = TensorDataset(test_data, test_labels)
 
-train_dataset = TensorDataset(train_data, train_labels)
-test_dataset = TensorDataset(test_data, test_labels)
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=BATCH,
+                              shuffle=True)
 
-train_loader = DataLoader(dataset=train_dataset,
-                          batch_size=BATCH,
-                          shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset,
+                             batch_size=BATCH,
+                             shuffle=False)
 
-test_loader = DataLoader(dataset=test_dataset,
-                         batch_size=BATCH,
-                         shuffle=False)
+    # Create model
+    model = FF(input_dim, hidden_dim, output_dim)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=LR)
 
-model = FF(input_dim, hidden_dim, output_dim)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=LR)
+    for epoch in range(EPOCHS):
+        for i, (data, labels) in enumerate(train_loader):
+            # Clear gradients w.r.t. parameters
+            optimizer.zero_grad()
 
-for epoch in range(EPOCHS):
-    for i, (data, labels) in enumerate(train_loader):
-        # Clear gradients w.r.t. parameters
-        optimizer.zero_grad()
+            # Forward pass to get output/logits
+            outputs = model(data)
+            # Calculate Loss: softmax --> cross entropy loss
+            loss = criterion(outputs, labels)
 
-        # Forward pass to get output/logits
-        outputs = model(data)
+            # Getting gradients w.r.t. parameters
+            loss.backward()
 
-        # Calculate Loss: softmax --> cross entropy loss
-        loss = criterion(outputs, labels)
+            # Updating parameters
+            optimizer.step()
 
-        # Getting gradients w.r.t. parameters
-        loss.backward()
+            ITER += 1
 
-        # Updating parameters
-        optimizer.step()
+            if ITER % 500 == 0:
+                # Calculate Accuracy
+                CORR = 0
+                TOTAL = 0
+                # Iterate through test dataset
+                for data, labels in test_loader:
+                    # Forward pass only to get logits/output
+                    outputs = model(data)
 
-        ITER += 1
+                    # Get predictions from the maximum value
+                    _, predicted = torch.max(outputs.data, 1)
 
-        if ITER % 500 == 0:
-            # Calculate Accuracy
-            CORR = 0
-            TOTAL = 0
-            # Iterate through test dataset
-            for data, labels in test_loader:
-                # Forward pass only to get logits/output
-                outputs = model(data)
+                    # Total number of labels
+                    TOTAL += labels.size(0)
+                    # Total correct predictions
+                    CORR += (predicted == labels).sum()
 
-                # Get predictions from the maximum value
-                _, predicted = torch.max(outputs.data, 1)
+                acc = 100 * CORR / TOTAL
+                if acc > highest_acc:
+                    highest_acc = acc
+                # Print Loss
+                # print(f'Iteration: {ITER}. Loss: {loss.item()}. Accuracy: {acc}')
+    scores.append(int(highest_acc))
+    print("mean at epoch", i, ":", round(mean(scores), 2))
 
-                # Total number of labels
-                TOTAL += labels.size(0)
-                # Total correct predictions
-                CORR += (predicted == labels).sum()
-
-            acc = 100 * CORR / TOTAL
-
-            # Print Loss
-            print(f'Iteration: {ITER}. Loss: {loss.item()}. Accuracy: {acc}')
+print("---------------")
+print("FINAL:")
+print("Overall:", round(mean(scores), 2))
+print("Standard deviation:", round(stdev(scores), 2))
--- examples/ala-lexibank-ff-torch.py
+++ examples/ala-lexibank-ff-torch.py
@@ -1,13 +1,14 @@
-from ala import get_wl, get_asjp, training_data, get_gb
-from ala import concept2vec, get_db
+from statistics import mean, stdev
 import numpy as np
-from collections import defaultdict
 import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
+from ala import get_wl, get_asjp, training_data, get_gb
+from ala import concept2vec, get_db
 
 
-results = defaultdict()
+RUNS = 100
+scores = []
 
 db = get_db("lexibank.sqlite3")
 converter = concept2vec(db, model="dolgo")
@@ -40,116 +41,124 @@ class FF(nn.Module):
         return out
 
 
-control = []
-train, test = training_data(
-        wordlists,
-        {k: v[0] for k, v in get_asjp().items()},
-        0.8,
-        3
-        )
+for i in range(RUNS):
+    highest_acc = 0
+    train, test = training_data(
+            wordlists,
+            {k: v[0] for k, v in get_asjp().items()},
+            0.8,
+            3
+            )
 
-fam2idx = {fam: i for i, fam in enumerate(train)}
-idx2fam = {v: k for k, v in fam2idx.items()}
-# Create training vector
-training_vec = []
-train_labels = []
+    fam2idx = {fam: i for i, fam in enumerate(train)}
+    idx2fam = {v: k for k, v in fam2idx.items()}
+    # Create training vector
+    training_vec = []
+    train_labels = []
 
-# train: {lng: [data]}
-for fam in train:
-    # iterate through data for each language
-    for lng, data in train[fam].items():
-        # set true label
-        true_labels = [0 for i in range(len(fam2idx))]
-        true_labels[fam2idx[fam]] = 1
+    # train: {lng: [data]}
+    for fam in train:
+        # iterate through data for each language
+        for lng, data in train[fam].items():
+            # set true label
+            true_labels = [0 for i in range(len(fam2idx))]
+            true_labels[fam2idx[fam]] = 1
 
-        # retrieve data to create converter
-        words = [[row[3], row[4].split()] for row in data.values()]
+            # retrieve data to create converter
+            words = [[row[3], row[4].split()] for row in data.values()]
 
-        # convert to long vector of sound classes
-        words_as_vector = converter(words)
-        training_vec.append(words_as_vector)
-        train_labels.append(true_labels)
+            # convert to long vector of sound classes
+            words_as_vector = converter(words)
+            training_vec.append(words_as_vector)
+            train_labels.append(true_labels)
 
-# Similar vector creation of items in test-set
-test_vec = []
-test_labels = []
-for fam in test:
-    # iterate through all items again
-    for lng, data in test[fam].items():
-        true_labels = [0 for i in range(len(fam2idx))]
-        true_labels = fam2idx[fam]
-        words = [[row[3], row[4].split()] for row in data.values()]
-        words_as_vector = converter(words)
-        test_vec.append(words_as_vector)
-        test_labels.append(true_labels)
+    # Similar vector creation of items in test-set
+    test_vec = []
+    test_labels = []
+    for fam in test:
+        # iterate through all items again
+        for lng, data in test[fam].items():
+            true_labels = [0 for i in range(len(fam2idx))]
+            true_labels = fam2idx[fam]
+            words = [[row[3], row[4].split()] for row in data.values()]
+            words_as_vector = converter(words)
+            test_vec.append(words_as_vector)
+            test_labels.append(true_labels)
 
-input_dim = len(words_as_vector)
-hidden_dim = 3*len(fam2idx)
-output_dim = len(fam2idx)
+    input_dim = len(words_as_vector)
+    hidden_dim = 3*len(fam2idx)
+    output_dim = len(fam2idx)
 
-LR = 0.002
-EPOCHS = 200
-BATCH = 10
-ITER = 0
+    LR = 0.002
+    EPOCHS = 200
+    BATCH = 10
+    ITER = 0
 
-train_data = torch.Tensor(np.array(training_vec))
-train_labels = torch.Tensor(np.array(train_labels))
-test_data = torch.Tensor(np.array(test_vec))
-test_labels = torch.Tensor(np.array(test_labels))
+    train_data = torch.Tensor(np.array(training_vec))
+    train_labels = torch.Tensor(np.array(train_labels))
+    test_data = torch.Tensor(np.array(test_vec))
+    test_labels = torch.Tensor(np.array(test_labels))
 
+    train_dataset = TensorDataset(train_data, train_labels)
+    test_dataset = TensorDataset(test_data, test_labels)
 
-train_dataset = TensorDataset(train_data, train_labels)
-test_dataset = TensorDataset(test_data, test_labels)
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=BATCH,
+                              shuffle=True)
 
-train_loader = DataLoader(dataset=train_dataset,
-                          batch_size=BATCH,
-                          shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset,
+                             batch_size=BATCH,
+                             shuffle=False)
 
-test_loader = DataLoader(dataset=test_dataset,
-                         batch_size=BATCH,
-                         shuffle=False)
+    model = FF(input_dim, hidden_dim, output_dim)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=LR)
 
-model = FF(input_dim, hidden_dim, output_dim)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=LR)
+    for epoch in range(EPOCHS):
+        for i, (data, labels) in enumerate(train_loader):
+            # Clear gradients w.r.t. parameters
+            optimizer.zero_grad()
 
-for epoch in range(EPOCHS):
-    for i, (data, labels) in enumerate(train_loader):
-        # Clear gradients w.r.t. parameters
-        optimizer.zero_grad()
+            # Forward pass to get output/logits
+            outputs = model(data)
 
-        # Forward pass to get output/logits
-        outputs = model(data)
+            # Calculate Loss: softmax --> cross entropy loss
+            loss = criterion(outputs, labels)
 
-        # Calculate Loss: softmax --> cross entropy loss
-        loss = criterion(outputs, labels)
+            # Getting gradients w.r.t. parameters
+            loss.backward()
 
-        # Getting gradients w.r.t. parameters
-        loss.backward()
+            # Updating parameters
+            optimizer.step()
 
-        # Updating parameters
-        optimizer.step()
+            ITER += 1
 
-        ITER += 1
+            if ITER % 500 == 0:
+                # Calculate Accuracy
+                CORR = 0
+                TOTAL = 0
+                # Iterate through test dataset
+                for data, labels in test_loader:
+                    # Forward pass only to get logits/output
+                    outputs = model(data)
 
-        if ITER % 500 == 0:
-            # Calculate Accuracy
-            CORR = 0
-            TOTAL = 0
-            # Iterate through test dataset
-            for data, labels in test_loader:
-                # Forward pass only to get logits/output
-                outputs = model(data)
+                    # Get predictions from the maximum value
+                    _, predicted = torch.max(outputs.data, 1)
 
-                # Get predictions from the maximum value
-                _, predicted = torch.max(outputs.data, 1)
+                    # Total number of labels
+                    TOTAL += labels.size(0)
+                    # Total correct predictions
+                    CORR += (predicted == labels).sum()
 
-                # Total number of labels
-                TOTAL += labels.size(0)
-                # Total correct predictions
-                CORR += (predicted == labels).sum()
+                acc = 100 * CORR / TOTAL
+                if acc > highest_acc:
+                    highest_acc = acc
+                # Print Loss
+                # print(f'Iteration: {ITER}. Loss: {loss.item()}. Accuracy: {acc}')
+    scores.append(int(highest_acc))
+    print("mean at epoch", i, ":", round(mean(scores), 2))
 
-            acc = 100 * CORR / TOTAL
-
-            # Print Loss
-            print(f'Iteration: {ITER}. Loss: {loss.item()}. Accuracy: {acc}')
+print("---------------")
+print("FINAL:")
+print("Overall:", round(mean(scores), 2))
+print("Standard deviation:", round(stdev(scores), 2))
