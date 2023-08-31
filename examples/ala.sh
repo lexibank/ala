ebf5894c6c1c26c10b603154642fb7fd38e7a090
Tarotis <frederic_blum@eva.mpg.de>
2023-08-31 15:53:42 +0200

    add results

---
+++ examples/ala.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --cpus-per-task 16
+#SBATCH -J results_ala
+#SBATCH --partition dlcegpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH --output=%x.%j.out
+#SBATCH --error=%x.%j.err
+
+python3 ala-grambank-ff-torch.py
+python3 ala-lexibank-ff-torch.py
+python3 ala-combined.py
