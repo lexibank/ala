079ccbabfdf4245dc87cbe3a1a5adad7cb6ec8ce
Frederic Blum <frederic_blum@eva.mpg.de>
2024-01-26 11:28:04 +0100

    new plots

--- examples/plots/results.R
+++ examples/plots/results.R
@@ -2,7 +2,6 @@ library(readr)
 library(dplyr)
 library(forcats)
 library(ggdist)
-library(ggExtra)
 library(gghalves)
 library(ggplot2)
 library(ggrepel)
@@ -11,7 +10,7 @@ library(viridis)
 gb_all <- read_tsv("../results/results_grambank_no_detailed.tsv") %>%
   mutate(Model='Grambank_all')
 
-lb_all <- read_tsv("../results/results_lexibank_no_detailed.tsv") %>%
+lb_all <- read_tsv("../results/results_lexibank_no_detailed_dolgo.tsv") %>%
   mutate(Model='Lexibank_all')
 
 gb_intersec <- read_tsv("../results/results_grambank_intersec_detailed.tsv") %>%
@@ -41,7 +40,7 @@ violin_complex <- per_model %>%
   geom_half_point(aes(fill=Model),side="l", range_scale=.25, alpha=.5, size=0.2) +
   stat_halfeye(aes(fill=Model), adjust=1, width=.6, color=NA, position=position_nudge(x=.15)) +
   coord_flip() +
-  scale_fill_viridis(discrete=TRUE) +
+  scale_fill_viridis(discrete=TRUE, end=0.9) +
   scale_y_continuous(limits=c(60, 99.5), breaks=c(60, 70, 80, 90, 100), 
                      name="Average Family Accuracy") +
   scale_x_discrete(label=NULL, name=NULL, breaks=NULL) +
@@ -67,10 +66,50 @@ scatter <-  per_family %>%
   # geom_smooth(method=lm , color="red", fill="#69b3a2", se=FALSE) +
   theme(legend.position="none") +
   facet_wrap(~Model)
-
 scatter
 # marginal boxplot
 # scatter <- ggMarginal(scatter, type="boxplot")
 ggsave("scatter.png", plot=scatter, dpi=300,
        width=3000, height=2000, units="px")
 
+#####################
+scope <- per_family %>% group_by(Model) %>%
+  summarise(langs=sum(Languages), fams=length(unique(Family)))
+
+scope_plot <- scope %>% 
+  ggplot(aes(x=fams, y=langs, fill=Model, label=Model)) +
+  geom_point(aes(size=5), shape=c(21, 23, 23, 22, 22, 24, 22), alpha=0.8, position=position_dodge(width=3)) +
+  scale_y_log10(limits = c(800, 5000), breaks=c(1000, 2000, 5000)) + 
+  geom_label_repel(max.overlaps=30, min.segment.length=unit(0, 'lines'), color="black",
+                   box.padding = unit(0.7, "lines")) +
+  theme(legend.position="none") +
+  scale_fill_viridis(discrete=TRUE, begin=0.3)
+scope_plot
+ggsave("scope.png", plot=scope_plot, dpi=300,
+       width=3000, height=2000, units="px")
+
+
+lang_acc <- scope %>% left_join(per_model) %>% group_by(Model) %>%
+  summarise(Accuracy=mean(Accuracy), langs=mean(langs), fams=mean(fams)) %>% 
+ggplot(aes(x=Accuracy, y=langs, fill=Model, label=Model)) +
+  geom_point(aes(size=5), shape=c(21, 23, 23, 22, 22, 24, 22), alpha=0.8) +
+  scale_y_log10(limits = c(800, 5000), breaks=c(1000, 2000, 5000)) + 
+  geom_label_repel(max.overlaps=30, min.segment.length=unit(0, 'lines'), color="black",
+                   box.padding = unit(0.7, "lines")) +
+  theme(legend.position="none") +
+  scale_fill_viridis(discrete=TRUE, begin=0.3)
+lang_acc
+ggsave("lang_acc.png", plot=lang_acc, dpi=300,
+       width=3000, height=2000, units="px")
+
+fam_acc <- scope %>% left_join(per_model) %>% group_by(Model) %>%
+  summarise(Accuracy=mean(Accuracy), langs=mean(langs), fams=mean(fams)) %>% 
+  ggplot(aes(x=Accuracy, y=fams, fill=Model, label=Model)) +
+  geom_point(aes(size=5), shape=c(21, 23, 23, 22, 22, 24, 22), alpha=0.8) +
+  geom_label_repel(max.overlaps=30, min.segment.length=unit(0, 'lines'), color="black",
+                   box.padding = unit(0.7, "lines")) +
+  theme(legend.position="none") +
+  scale_fill_viridis(discrete=TRUE, begin=0.3)
+fam_acc
+ggsave("fam_acc.png", plot=fam_acc, dpi=300,
+       width=3000, height=2000, units="px")
