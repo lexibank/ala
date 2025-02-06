8ee95ff2a4518e0805ad54ae5493d2dbb4764fcf
FredericBlum <frederic_blum@eva.mpg.de>
2025-02-06 13:25:35 +0100

    update results without seed

--- examples/ala.sh
+++ examples/ala.sh
@@ -7,11 +7,11 @@
 #SBATCH --output=%x.%j.out
 #SBATCH --error=%x.%j.err
 
-# python3 -u ala_ff_torch.py --database=lexibank
-# python3 -u ala_ff_torch.py --database=grambank
-# python3 -u ala_ff_torch.py --database=combined
-# python3 -u ala_ff_torch.py --database=asjp
-python3 -u ala_ff_torch.py --database=lexibank -experiment
+python3 -u ala_ff_torch.py --database=lexibank
+python3 -u ala_ff_torch.py --database=grambank
+python3 -u ala_ff_torch.py --database=combined
+python3 -u ala_ff_torch.py --database=asjp
+#python3 -u ala_ff_torch.py --database=lexibank -experiment
 # python3 -u ala_ff_torch.py --database=asjp -experiment
 #python3 -u ala_ff_torch.py --database=grambank -experiment
 #python3 -u ala_ff_torch.py --database=combined -experiment
